9f0e90054750dbfac575591457c643324970c137
AndrewPark1234567890 <andrewdpark21@gmail.com>
2015-08-07 18:29:56 -0700

    Stuff
    Stuff

---
+++ Level1.no/src/Timer.java
@@ -0,0 +1,11 @@
+public class Timer {
+	String color;
+	int weight;
+	public Timer(String color, int weight) {
+		this.color = color;
+		this.weight = weight;
+	}
+	public String toString(){
+		return"THis timer is lame. Its stats dont matter but whatever here it is: Color " + color + " Weight: " + weight + ". There. Are you happy??? ARE YOU??? Glad you wasted ur lyfe.";
+	}
+}
